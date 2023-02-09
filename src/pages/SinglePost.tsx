7b07ee745dcbaa70688b3190a2da61f86917da3e
Swappnet <advance11ua@gmail.com>
2023-02-09 13:48:55 +0200

    init

--- src/pages/SinglePost.tsx
+++ src/pages/SinglePost.tsx
@@ -34,94 +34,89 @@ const SinglePostWrapper = () => {
     return () => clearTimeout(timer);
   };
 
-  const postData: Data | undefined = posts.find(
-    (item) => String(item.id) === id
-  );
-
-  if (postData != undefined) {
-    return (
-      <div className="singlepost-wrapper">
-        {isFetching === 0 ? (
-          <div className="singlepost-fetching-wrapper" />
-        ) : isFetching === 1 ? (
-          <>
-            {" "}
-            <div className="singlepost-wrapper--main">
-              <div className="singlepost-image--container">
-                <img
-                  data-src={postData.image}
-                  className="singlepost-image lazyload"
-                  alt={`${postData.title} image`}
-                  title={`${postData.title} image`}
-                  aria-label={`${postData.title} image`}
-                />
+  const postData: any = posts.find((item) => String(item.id) === id);
+  return (
+    <div className="singlepost-wrapper">
+      {isFetching === 0 ? (
+        <div className="singlepost-fetching-wrapper" />
+      ) : isFetching === 1 ? (
+        <>
+          {" "}
+          <div className="singlepost-wrapper--main">
+            <div className="singlepost-image--container">
+              <img
+                data-src={postData.image}
+                className="singlepost-image lazyload"
+                alt={`${postData.title} image`}
+                title={`${postData.title} image`}
+                aria-label={`${postData.title} image`}
+              />
+            </div>
+            <div className="singlepost-content">
+              <div className="singlepost--info-box">
+                <div className="info-box--image--wrapper">
+                  <img
+                    className="info-box--image lazyload"
+                    title="Creator Image"
+                    aria-label="Creator Image"
+                    alt=""
+                    data-src={
+                      postData.authorUID === "iaBmTalxlBOJ8Fnat8klUtY5CJI3"
+                        ? swappnetFull
+                        : mrflockerFull
+                    }
+                  />
+                </div>
+                <div className="info-box--content">
+                  <p className="info-box--content-author">
+                    Created By
+                    <span className="info-box--content-author--name">
+                      {postData.authorUID === "iaBmTalxlBOJ8Fnat8klUtY5CJI3"
+                        ? " Swappnet"
+                        : " MrFLocker"}
+                    </span>
+                  </p>
+                  <p className="info-box--content-date">{postData.date}</p>
+                </div>
               </div>
-              <div className="singlepost-content">
-                <div className="singlepost--info-box">
-                  <div className="info-box--image--wrapper">
-                    <img
-                      className="info-box--image lazyload"
-                      title="Creator Image"
-                      aria-label="Creator Image"
-                      alt=""
-                      data-src={
-                        postData.authorUID === "iaBmTalxlBOJ8Fnat8klUtY5CJI3"
-                          ? swappnetFull
-                          : mrflockerFull
-                      }
-                    />
-                  </div>
-                  <div className="info-box--content">
-                    <p className="info-box--content-author">
-                      Created By
-                      <span className="info-box--content-author--name">
-                        {postData.authorUID === "iaBmTalxlBOJ8Fnat8klUtY5CJI3"
-                          ? " Swappnet"
-                          : " MrFLocker"}
-                      </span>
-                    </p>
-                    <p className="info-box--content-date">{postData.date}</p>
-                  </div>
-                </div>
-                <p className="singlepost-content--title">{postData.title}</p>
-                <p className="singlepost-content--description">
-                  {postData.content}
-                </p>
-                <Link
-                  role="button"
-                  title="Back to Posts"
-                  aria-label="Back to Posts"
-                  className="back-link"
-                  to="/Posts"
+              <p className="singlepost-content--title">{postData.title}</p>
+              <p className="singlepost-content--description">
+                {postData.content}
+              </p>
+              <Link
+                role="button"
+                title="Back to Posts"
+                aria-label="Back to Posts"
+                className="back-link"
+                to="/Posts"
+              >
+                Back to Posts{" "}
+                <img alt="" className="back-link--icon" src={arrowL} />
+              </Link>
+              <div className="singlepost-content--footer">
+                {copySuccess ? (
+                  <div className="copied--wrapper">Copied</div>
+                ) : (
+                  ""
+                )}
+                <button
+                  tabIndex={0}
+                  className="share-btn"
+                  title="Share Post"
+                  aria-label="Share Post"
+                  onClick={() => handleCopyToBuffer()}
                 >
-                  Back to Posts{" "}
-                  <img alt="" className="back-link--icon" src={arrowL} />
-                </Link>
-                <div className="singlepost-content--footer">
-                  {copySuccess ? (
-                    <div className="copied--wrapper">Copied</div>
-                  ) : (
-                    ""
-                  )}
-                  <button
-                    tabIndex={0}
-                    className="share-btn"
-                    title="Share Post"
-                    aria-label="Share Post"
-                    onClick={() => handleCopyToBuffer()}
-                  >
-                    <i className="gg-share"></i>
-                  </button>
-                </div>
+                  <i className="gg-share"></i>
+                </button>
               </div>
             </div>
-          </>
-        ) : (
-          <Failed />
-        )}
-      </div>
-    );
-  }
+          </div>
+        </>
+      ) : (
+        <Failed />
+      )}
+    </div>
+  );
 };
 
 export default SinglePostWrapper;
